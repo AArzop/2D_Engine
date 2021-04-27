87bbe1c66f9b98f87cae50596ea85c06ffef3a00
AArzop <37124191+AArzop@users.noreply.github.com>
2021-04-27 10:45:23 +0200

    Create Component loader

--- 2D_Engine/2D_Engine/Private/Management/Load/ILoader.h
+++ 2D_Engine/2D_Engine/Private/Management/Load/ILoader.h
@@ -9,12 +9,23 @@ namespace engine
 		class ShapeList;
 	}
 
+	namespace gameplay
+	{
+		class Entity;
+	}
+
 	namespace management::load
 	{
 		class IShapeLoader
 		{
 		public:
 			virtual void Instanciate(graphics::ShapeList& shapeList, std::string json) = 0;
+		};
+
+		class IComponentLoader
+		{
+		public:
+			virtual void Instanciate(gameplay::Entity& entity, std::string json) = 0;
 		};
 	}
 }
